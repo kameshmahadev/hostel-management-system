d5784e4163d95729e9b3cb0be5ebda0ed4bfe191
Kameshmahadev <mkameshdev@gmail.com>
2025-05-11 22:45:11 +0530

    commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const { register, login } = require('../controllers/userController');
 
-// User Routes
 router.post('/register', register);
 router.post('/login', login);
 
