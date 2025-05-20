5f849b626646a29dff33e8367f0c9faa4a7ec63e
Kameshmahadev <mkameshdev@gmail.com>
2025-05-21 00:25:19 +0530

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,44 +1,18 @@
+// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
+const {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
 
-const { register, login, createUser, updateUser } = require('../controllers/userController');
-const { body, validationResult } = require('express-validator');
+const { protect, authorizeRoles } = require('../middleware/authMiddleware');
 
-router.post('/register', register);
-router.post('/login', login);
-
-router.post(
-  '/',
-  [
-    body('username').notEmpty().withMessage('Username is required'),
-    body('email').isEmail().withMessage('Valid email required'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
-  ],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
-  createUser
-);
-
-router.put(
-  '/:id',
-  [
-    body('username').optional().notEmpty().withMessage('Username cannot be empty'),
-    body('email').optional().isEmail().withMessage('Valid email required'),
-    // Add more validations as needed
-  ],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
-  updateUser
-);
+router.get('/', protect, authorizeRoles('admin'), getAllUsers);
+router.get('/:id', protect, getUserById);
+router.put('/:id', protect, updateUser);
+router.delete('/:id', protect, authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
